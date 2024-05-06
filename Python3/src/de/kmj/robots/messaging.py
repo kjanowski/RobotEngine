f5eaac10bf03979f933ec6cd3648bce0c8dd7012
kathrin <kathrin.janowski@informatik.uni-augsburg.de>
2024-05-06 18:37:50 +0200

    Python3: added MessageClient to messaging.py, updated MessageServer

--- Python3/src/de/kmj/robots/messaging.py
+++ Python3/src/de/kmj/robots/messaging.py
@@ -76,24 +76,24 @@ class StatusMessage:
 
 
 ########################################################################
-# UDP-Server which communicates with the control application           #
+# UDP Server which communicates with the control application           #
 ########################################################################
 class MessageServer(threading.Thread):
 
     #===================================================================
     # Constructor of Server 
     #===================================================================
-    def __init__(self, ip, port, buffersize, timeout, engine):
+    def __init__(self, local_host, local_port, buffer_size, timeout, engine):
         threading.Thread.__init__(self)
-        self.daemon = False
         self.engine = engine
         
-        self.proxy_ip = ip
-        self.proxy_port = port
-        self.buffersize = buffersize
+        self.local_host = local_host
+        self.local_port = local_port
+        self.buffer_size = buffer_size
         self.timeout = timeout
         self.debugOutput = False
         
+        self.__reset__() 
     
     #===================================================================
     # Reset local attributes
@@ -119,6 +119,7 @@ class MessageServer(threading.Thread):
                     print("[MessageServer] Sending StatusMessage: ", status.tostring())
             except Exception as exc:
                 print("[MessageServer] Cannot send StatusMessage: ", exc)
+    
     #===================================================================
     # Stop the Server: Close the Socket and Reset.
     #===================================================================
@@ -127,8 +128,10 @@ class MessageServer(threading.Thread):
         if(self.serverSock): 
             self.serverSock.close()
             print("[MessageServer] Closing Server Socket")
-        print("[MessageServer] Aborting Server Thread")
-        self.isRunning = False
+        # Reset the Member Fields
+        self.__reset__()
+        print("[MessageServer] Stopping Server Thread")
+        self._Thread__stop() 
      
     #===================================================================
     # Server.start(): The main method of the server thread.
@@ -142,10 +145,9 @@ class MessageServer(threading.Thread):
             if self.debugOutput:
                 print("[MessageServer] Creating Socket")
             self.serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.serverSock.settimeout(self.timeout)
             if self.debugOutput:
-                print("[MessageServer] Binding Server Socket to ", self.proxy_ip ,":", self.proxy_port)
-            self.serverSock.bind((self.proxy_ip, self.proxy_port))
+                print("[MessageServer] Binding Server Socket to ", self.local_host ,":", self.local_port)
+            self.serverSock.bind((self.local_host, self.local_port))
 
             
             # Set The Running Flag
@@ -159,22 +161,101 @@ class MessageServer(threading.Thread):
             #--------------------------------------------------------------------
             while self.isRunning:
                 try:
-                    self.clientData, self.clientAddr = self.serverSock.recvfrom(self.buffersize)
+                    self.clientData, self.clientAddr = self.serverSock.recvfrom(self.buffer_size)
                     if self.debugOutput:
                         print("[MessageServer] received ", len(self.clientData), " from address ", self.clientAddr, ": ", self.clientData)
                     
                     cmd = CommandMessage("unknown", "unknown")
                     cmd.parse(self.clientData)                        
                     self.engine.execute(cmd)
-                except socket.timeout as exc:
-                    answer = 42 #ignore
                 except Exception as exc:
                     print("[MessageServer] EXCEPTION: ", exc)
                     
         except Exception as exc:
             print("[MessageServer] Server Thread Exception ", exc)
-            print("[MessageServer] Stopping Server Thread")
+            print("[MessageServer] Aborting Server Thread")
             self.isRunning = False
         
-        print("MessageServer stopped.")
 
+########################################################################
+# UDP Client which communicates with the RobotEngine                   #
+########################################################################
+class MessageClient(threading.Thread):
+
+    #===================================================================
+    # Constructor of Client 
+    #===================================================================
+    def __init__(self, local_host, local_port, remote_host, remote_port, buffer_size, callback):
+        threading.Thread.__init__(self)
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.buffer_size = buffer_size
+        self.local_host = local_host
+        self.local_port = local_port
+        self.remote_host = remote_host
+        self.remote_port = remote_port
+        self.callback = callback
+
+        self.debugOutput = False
+    
+
+    #===================================================================
+    # Sends a command message to the RobotEngine.
+    #===================================================================
+    def send(self, command):
+        self.socket.sendto(command.tostring(), (self.remote_host, self.remote_port))
+    
+    #===================================================================
+    # Stop the Client: Close the Socket and Reset.
+    #===================================================================
+    def stop(self):            
+        # Close The Socket    
+        if(self.socket): 
+            self.socket.close()
+            print("[MessageClient] Closing Socket")
+        print("[MessageClient] Stopping Client Thread")
+        self._Thread__stop() 
+     
+    #===================================================================
+    # Client.start(): The main method of the client thread.
+    #===================================================================
+    def run(self):          
+
+        try:
+            #--------------------------------------------------------------------
+            # Create and bind the socket                                         
+            #--------------------------------------------------------------------
+            if self.debugOutput:
+                print("[MessageClient] Creating Socket")
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            if self.debugOutput:
+                print("[MessageClient] Binding Client Socket to ", self.local_host ,":", self.local_port)
+            self.socket.bind((self.local_host, self.local_port))
+
+            
+            # Set The Running Flag
+            self.isRunning = True;     
+            if self.debugOutput:
+                print("[MessageClient] Running and waiting for message")
+
+            #--------------------------------------------------------------------
+            # Main loop where the communication happens.                         
+            # Client is in infinite loop until stopped by the parent application.
+            #--------------------------------------------------------------------
+            while self.isRunning:
+                try:
+                    data = self.socket.recv(self.buffer_size)
+                    
+                    if self.debugOutput:
+                        print("[MessageClient] received ", len(self.data), ": ", data)
+                    
+                    status = StatusMessage("unknown", "unknown")
+                    status.parse(data)                        
+                    self.callback(status)
+                except Exception as exc:
+                    print("[MessageClient] EXCEPTION: ", exc)
+                    
+        except Exception as exc:
+            print("[MessageClient] Client Thread Exception ", exc)
+            print("[MessageClient] Aborting Client Thread")
+            self.isRunning = False
